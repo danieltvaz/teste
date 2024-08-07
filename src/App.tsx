f452c6fd02be2e4796f1511aec6ba341f802ec34
danieltvaz <danieltostes@live.com>
2024-08-07 17:46:04 -0300

    chore: change file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { fieldTypes, fields, forms, sections } from "./types/livecode_form";
 
-import Form1 from "./components/form-1";
+import Form from "./components/form";
 
 function App() {
   return (
     <>
       {forms.map((form, index) => (
-        <Form1 key={`form-${index}`} form={form} />
+        <Form key={`form-${index}`} form={form} />
       ))}
     </>
   );
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,13 +1,11 @@
 import * as Styled from "./styles";
 
-import { fieldTypes, fields, sections } from "../../types/livecode_form";
+import { fields, sections } from "../../types/livecode_form";
 
 import { Form1Props } from "./types";
-import { useState } from "react";
+import React from "react";
 
 export default function Form1({ form }: Form1Props) {
-  const [inputsData, setinputsData] = useState({});
-
   function getSectionDataById(sectionId: number) {
     return sections.filter((section) => section.Id === sectionId)[0];
   }
@@ -19,26 +17,26 @@ export default function Form1({ form }: Form1Props) {
   return (
     <Styled.FormWrapper>
       <Styled.FormTitle>{form.Title}</Styled.FormTitle>
-      {form.SectionIds.map((sectionId) => {
+      {form.SectionIds.map((sectionId, index) => {
         const sectionData = getSectionDataById(sectionId);
 
         return (
-          <Styled.FormSection>
+          <Styled.FormSection key={`section-${sectionId}-${index}`}>
             <Styled.SectionTitle>{sectionData.Title}</Styled.SectionTitle>
-            {sectionData.FieldIds?.map((fieldId) => {
+            {sectionData.FieldIds?.map((fieldId, index) => {
               const fieldData = getFieldDataById(fieldId);
 
               return (
-                <>
+                <React.Fragment key={`field-${fieldId}-${index}`}>
                   <Styled.Input title={fieldData.Title} />
-                </>
+                </React.Fragment>
               );
             })}
-            {sectionData.SubSectionIds?.map((subSectionId) => {
+            {sectionData.SubSectionIds?.map((subSectionId, index) => {
               const subSectionData = getSectionDataById(subSectionId);
 
               return (
-                <Styled.FormSection>
+                <Styled.FormSection key={`subsection-${subSectionId}-${index}`}>
                   <Styled.SubsectionTitle>{subSectionData.Title}</Styled.SubsectionTitle>
                 </Styled.FormSection>
               );
